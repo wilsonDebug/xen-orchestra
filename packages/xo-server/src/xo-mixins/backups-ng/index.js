0b69950e83c84876c311656d17cee3358f9b5a95
Julien Fontanet <julien.fontanet@isonoe.net>
2018-05-22 19:41:52 +0200

    feat(xo-server/backup NG): new setting copyRetention
    This setting which defaults to exportRetention is used for DR and CR.
    
    Fixes #2895

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -49,6 +49,7 @@ type ReportWhen = 'always' | 'failure' | 'never'
 type Settings = {|
   concurrency?: number,
   deleteFirst?: boolean,
+  copyRetention?: number,
   exportRetention?: number,
   reportWhen?: ReportWhen,
   snapshotRetention?: number,
@@ -143,11 +144,12 @@ const defaultSettings: Settings = {
   snapshotRetention: 0,
   vmTimeout: 0,
 }
-const getSetting = (
+const getSetting = <T>(
   settings: $Dict<Settings>,
   name: $Keys<Settings>,
-  ...keys: string[]
-): any => {
+  keys: string[],
+  defaultValue?: T
+): T | any => {
   for (let i = 0, n = keys.length; i < n; ++i) {
     const objectSettings = settings[keys[i]]
     if (objectSettings !== undefined) {
@@ -481,14 +483,10 @@ export default class BackupNg {
               logger,
               taskId
             )
-            const vmTimeout: number = getSetting(
-              job.settings,
-              'vmTimeout',
+            const vmTimeout: number = getSetting(job.settings, 'vmTimeout', [
               uuid,
               scheduleId,
-              logger,
-              taskId
-            )
+            ])
             if (vmTimeout !== 0) {
               p = pTimeout.call(p, vmTimeout)
             }
@@ -737,18 +735,31 @@ export default class BackupNg {
     const exportRetention: number = getSetting(
       settings,
       'exportRetention',
-      scheduleId
+      [scheduleId],
+      0
     )
     const snapshotRetention: number = getSetting(
       settings,
       'snapshotRetention',
-      scheduleId
+      [scheduleId],
+      0
     )
 
     if (exportRetention === 0) {
       if (snapshotRetention === 0) {
         throw new Error('export and snapshots retentions cannot both be 0')
       }
+    }
+
+    const copyRetention: number = getSetting(
+      settings,
+      'copyRetention',
+      [scheduleId],
+      exportRetention
+    )
+
+    if ((copyRetention === 0) !== (exportRetention === 0)) {
+      throw new Error('both or neither copy and export rententions should be 0')
     }
 
     if (
@@ -921,7 +932,7 @@ export default class BackupNg {
                 const sr = xapi.getObject(srId)
 
                 const oldVms = getOldEntries(
-                  exportRetention,
+                  copyRetention,
                   listReplicatedVms(xapi, scheduleId, srId, vmUuid)
                 )
 
@@ -1147,7 +1158,7 @@ export default class BackupNg {
                 const sr = xapi.getObject(srId)
 
                 const oldVms = getOldEntries(
-                  exportRetention,
+                  copyRetention,
                   listReplicatedVms(xapi, scheduleId, srId, vmUuid)
                 )
 
